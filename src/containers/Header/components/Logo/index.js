8f83c5bdd1968fc601d5d62b00d8263cd36930dc
sebek78 <sebasporek@gmail.com>
2020-05-23 15:30:31 +0200

    Modify Logo component

--- src/containers/Header/components/Logo/index.js
+++ src/containers/Header/components/Logo/index.js
@@ -1,13 +1,29 @@
 import React from "react";
 import logo from "../../../../img/favicon.png";
+import styled from "styled-components";
+import COLORS from "../../../../utils/colors";
 
-const Logo = () => {
-  return (
-    <div className="header__logo">
-      <img src={logo} />
-      <p>Moje przepisy</p>
-    </div>
-  );
-};
+const StyledLogo = styled.div`
+  height: 64px;
+  width: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  font-size: 24px;
+  padding-right: 4px;
+  line-height: 1;
+  color: ${COLORS.primary};
+`;
+
+const Logo = () => (
+  <StyledLogo>
+    <img src={logo} />
+    <p>
+      Rubinowe
+      <br />
+      przepisy
+    </p>
+  </StyledLogo>
+);
 
 export default Logo;
---
+++ src/utils/colors.js
@@ -0,0 +1,11 @@
+const COLORS = {
+  bgColor: "#eaeaea",
+  bgColorAlpha: "rgba(234, 234, 234, 0.9)",
+  primary: "#a91401",
+  primaryDark: "#820c02",
+  primaryLight: "#d51f06",
+  secondary: "#de3f24",
+  secondaryLight: "#eda895",
+};
+
+export default COLORS;
